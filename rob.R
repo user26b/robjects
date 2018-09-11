6a8e34bdf52b7964c1e1af6076e66a10d087ea20
Pedolin Dario A <a80848243@ags-vzhrec-3020.evdad.admin.ch>
2018-09-11 12:25:44 +0200

    produkte und impacts test S4 classes

--- rob.R
+++ rob.R
@@ -111,3 +111,126 @@ alice <-Person(name = "Alice", age = 40)
 john <- Employee(name = "John", age = 20, boss = alice)
 
 john@boss@age
+
+
+
+
+# 
+# lca beispiele
+# 
+
+
+
+# 
+# class unit
+# 
+# 
+setClass("Einheit",
+         slots = list(name = "character"))
+
+#  constructor
+Einheit <- function(name) {
+  new("Einheit", name = name)
+}
+
+# kg <- Einheit("kg")
+# g <- Einheit("g")
+# kj <- Einheit("kJ")
+# kcal <- Einheit("kcal")
+# chf <- Einheit("CHF")
+
+
+
+# 
+# class functional unit
+# 
+# 
+setClass("Fu",
+         slots = list(name = "character", unit = "Einheit"))
+
+#  constructor
+Fu <- function(name, unit){  
+  new("Fu", name = name, unit = unit)
+}
+
+# verdE <- Fu("verdaubareEnergie", kcal)
+# protein <- Fu("protein", g)
+
+
+# 
+# class environmental impact
+# 
+# 
+setClass("EnvImp",
+         slots = list(name = "character", unit = "Einheit"))
+
+#  constructor
+EnvImp <- function(name, unit){  
+  new("EnvImp", name = name, unit = unit)
+}
+
+# ghg <- EnvImp("GHG", kg)
+# terrtoc <- EnvImp("TerrestTox", g)
+
+
+
+#
+# class mengeEinheit
+#
+#
+#
+setClass("MengeEinheiten",
+         slots = list(value = "list", unit = "list"))
+
+#  constructor
+MengeEinheiten <- function(values, units){
+  new("MengeEinheiten", value = values, unit = units)
+}
+
+
+
+# 
+# class product
+# 
+# 
+setClass("Produkt",
+         slots = list(name = "character", fuList = "MengeEinheiten", impList = "MengeEinheiten"))
+
+#  constructor
+Produkt <- function(name, fuList, impList){  
+  new("Produkt", name = name, fuList = fuList, impList = impList)
+}
+
+
+
+setGeneric("getFuMenge", function(x, y) {
+  standardGeneric("getFuMenge")
+})
+
+setMethod("getFuMenge", 
+          c(x = "Produkt", y = "Fu"),
+          function(x, y) {
+            x@fuList@value[which(x@fuList@unit[[2]] == y)]
+          }
+)
+
+
+# init instances
+kg <- Einheit("kg")
+g <- Einheit("g")
+kj <- Einheit("kJ")
+kcal <- Einheit("kcal")
+chf <- Einheit("CHF")
+
+verdE <- Fu("verdaubareEnergie", kcal)
+protein <- Fu("protein", g)
+
+ghg <- EnvImp("GHG", kg)
+terrtoc <- EnvImp("TerrestTox", g)
+
+
+milch <- Produkt("Milch", MengeEinheiten(list(122, 11), list(verdE, protein)), MengeEinheiten(list(567, 1.3), list(ghg, terrtoc)))
+
+milch@impList
+
+milch@impList@unit[[2]]@name
